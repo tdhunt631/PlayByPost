011a7ff2dd29638ff71d7c8a7eba4c75e17ed9b2
Tdhunt631 <tdhunt631@gmail.com>
2016-01-23 10:29:46 -0700

    fixed path to templates in settings.py

--- PlayByPost/settings.py
+++ PlayByPost/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'PlayByPost.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['PlayByPost/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, 'PlayByPost/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
